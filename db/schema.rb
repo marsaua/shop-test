b0e73255e3a834730b5f0423afd0e9d2b9b8f9dc
marsaua001 <marsaua001@gmail.com>
2026-09-08 11:38:30 +0300

    Clean up schema.rb: remove orphaned cart_items/order_items tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_09_08_082955) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_08_082146) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -42,20 +42,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_08_082955) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "cart_items", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.bigint "product_id", null: false
-    t.datetime "updated_at", null: false
-    t.index ["product_id"], name: "index_cart_items_on_product_id"
-  end
-
-  create_table "order_items", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.bigint "product_id", null: false
-    t.datetime "updated_at", null: false
-    t.index ["product_id"], name: "index_order_items_on_product_id"
-  end
-
   create_table "products", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "description"
@@ -80,6 +66,4 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_08_082955) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "cart_items", "products"
-  add_foreign_key "order_items", "products"
 end
